644b1874129afd14d519acc8f4fc2cf5c4a76ce7
Mark Suter <msuter@live.elida.k12.oh.us>
2021-03-02 08:54:31 -0500

    New Copy Paste

---
+++ Assets/Scripts/AddForceController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceController : MonoBehaviour
+{
+    public float thrust = 1.0f;
+    public Rigidbody2D rb;
+    public float rotationSpeed = 100.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * rotationSpeed);
+
+        
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.up * thrust * Input.GetAxis("Vertical"));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            //Debug.Log("You hit an enemy");
+            Destroy(other.gameObject);
+        }
+    }
+}
